2d5b21c302c74184cd3968c95d292caca0044371
heroapoorva <HEROAPOORVA@GMAIL.COM>
2019-07-10 17:41:02 -0400

    Update ml.py

--- ml.py
+++ ml.py
@@ -1,37 +1,55 @@
-from functions import *
-with open('clean.json') as json_file:  
-    lines=json_file.readlines()
+from classification_functions import *
+def main():
+    with open(sys.argv[1]) as json_file:  
+        lines=json_file.readlines()
+    with open(sys.argv[2]) as json_file:  
+        data = json.load(json_file)
+    with open(sys.argv[3]) as read_file:  
+        keywords = read_file.readlines()
+    print("Read the news files and the dictionary")
+    pool = mp.Pool(processes=mp.cpu_count())
+    keywords=pool.map(remove_junk, (keywords[j] for j in range(len(keywords)) ))
+    keywords=pool.map(remove_stop_words,(keywords[j] for j in range(len(keywords)) ))
+    keywords=pool.map(get_lemmatized_text,(keywords[j] for j in range(len(keywords)) ))
+    print("Names are cleared")
+    
+    (top_dict,index_dict)=get_top(int(sys.argv[4]),data)
+    print("got dictionary")
+    
     lines=dictionary_array(lines)
-with open('DowJones30.txt') as fh:
-    com_nam=fh.readlines()
-with open('dict.json') as json_file:  
-    data = json.load(json_file)
+    print("Parsing finished!")
+    
+    indices=index_matrix(keywords,lines)
+    print("Got indices")
 
-pool = mp.Pool(processes=mp.cpu_count())
-pro_nam=pool.map(remove_junk, (com_nam[j] for j in range(len(com_nam)) ))
-pro_nam=pool.map(remove_stop_words,(pro_nam[j] for j in range(len(pro_nam)) ))
-pro_nam=pool.map(get_lemmatized_text,(pro_nam[j] for j in range(len(pro_nam)) ))
-print("Names are cleared")
+    y=[]
+    for i in range(len(indices)):
+        for j in range(len(indices[i])):
+            y.append(i)
+    y=np.asarray(y)
+    print("Got y")
 
-(top_dict,index_dict)=get_top(20000,data)
-print("Got dictionary")
+    indices=flatten_2d(indices)
+    X=frequency_matrix(subarray(lines,indices), top_dict,index_dict)
+    print("Got X")
 
-indices=index_matrix(pro_nam,lines)
-print("Got indices")
+    clf = naive_bayes(len(keywords))
+    clf = clf.fit(X, y)
+    print("Fitting Done")
+    answers=clf.predict([frequency_vector(lines[0]["text"], top_dict, index_dict)])
+    fh=open(sys.argv[5],"w")
+    for i in answers:
+        fh.write(str(i))
+        fh.write("\n")
+    fh.close()
 
-y=[]
-for i in range(len(indices)):
-    for j in range(len(indices[i])):
-        y.append(i)
-y=np.asarray(y)
-print("Got y")
-
-indices=flatten_2d(indices)
-X=frequency_matrix(subarray(lines,indices), top_dict,index_dict)
-print("Got X")
-
-clf = naive_bayes(len(pro_nam))
-clf = clf.fit(X, y)
-Print("Fitting Done")
-answers=clf.predict([frequency_vector(lines[0]["text"], top_dict, index_dict)])
-print(answers)
+if __name__ == '__main__':
+    '''
+    There should be at max 6 inputs, 
+    First the cleaned json file, 
+    Second the dictionary file contaning the words,
+    Third the keywords file, each separated by a line.
+    Fourth the number of topwords to use
+    Fifth the output file for the classification values.
+    '''
+    main()
