f2bcf7de79321569bbe868aa42fdb5e3c86ea23b
heroapoorva <HEROAPOORVA@GMAIL.COM>
2019-07-10 17:40:41 -0400

    Update Further.py

--- Further.py
+++ Further.py
@@ -1,29 +1,45 @@
-
-from functions import *
-with open('clean.json') as json_file:  
-    lines=json_file.readlines()
+from outlier_functions import *
+def main():
+    with open(sys.argv[1]) as json_file:  
+        lines=json_file.readlines()
+    with open(sys.argv[2]) as json_file:  
+        data = json.load(json_file)
+    with open(sys.argv[3]) as read_file:  
+        keywords = read_file.readlines()
+    print("Read the news files and the dictionary")
+    pool = mp.Pool(processes=mp.cpu_count())
+    keywords=pool.map(remove_junk, (keywords[j] for j in range(len(keywords)) ))
+    keywords=pool.map(remove_stop_words,(keywords[j] for j in range(len(keywords)) ))
+    keywords=pool.map(get_lemmatized_text,(keywords[j] for j in range(len(keywords)) ))
+    print("Names are cleared")
+    
+    (top_dict,index_dict)=get_top(int(sys.argv[4]),data)
+    print("got dictionary")
+    
     lines=dictionary_array(lines)
-with open('DowJones30.txt') as fh:
-    com_nam=fh.readlines()
-with open('dict.json') as json_file:  
-    data = json.load(json_file)
-
-pool = mp.Pool(processes=mp.cpu_count())
-pro_nam=pool.map(remove_junk, (com_nam[j] for j in range(len(com_nam)) ))
-pro_nam=pool.map(remove_stop_words,(pro_nam[j] for j in range(len(pro_nam)) ))
-pro_nam=pool.map(get_lemmatized_text,(pro_nam[j] for j in range(len(pro_nam)) ))
-print("Names are cleared")
-
-(top_dict,index_dict)=get_top(20000,data)
-print("got dictionary")
-
-indices=index_matrix(pro_nam,lines)
-fh=open("value.txt","w")
-for i in range(len(indices)):
-    matrix=subarray(lines,indices[i])
-    values=sliding_window_tfidf(matrix,top_dict,index_dict,20)
-    for j in values:
-        fh.write(str(j))
-        fh.write(" ")
-    fh.write("\n")
-fh.close()
+    print("Parsing finished!")
+    
+    indices=index_matrix(keywords,lines)
+    print("Got indices")
+    
+    fh=open(sys.argv[6],"w")
+    for i in range(len(indices)):
+        matrix=subarray(lines,indices[i])
+        values=sliding_window_tfidf(matrix,top_dict,index_dict,int(sys.argv[5]))
+        for j in values:
+            fh.write(str(j))
+            fh.write(" ")
+        fh.write("\n")
+    fh.close()
+    
+if __name__ == '__main__':
+    '''
+    There should be at max 6 inputs, 
+    First the cleaned json file, 
+    Second the dictionary file contaning the words,
+    Third the keywords file, each separated by a line.
+    Fourth the number of topwords to use
+    Fifth the window size 
+    Lastly the output file for the novelty values.
+    '''
+    main()
