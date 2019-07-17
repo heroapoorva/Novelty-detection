989314dd593cc1902867ce1c320cf0a583128b11
heroapoorva <HEROAPOORVA@GMAIL.COM>
2019-07-17 13:18:00 -0400

    Update outlier_function.py

--- outlier_function.py
+++ outlier_function.py
@@ -1,6 +1,6 @@
 from novelty_function import *
 
-# Given 2 list of "pro_nam" of strings and "lines" of dictionaries. The 
+# Given 2 list of "keywords" of strings and "lines" of dictionaries. The 
 # function returns a 2D array ith array contains indices of occurances of
 # pro_nam[i] in lines['title'].
 def index_matrix(keywords,lines):
@@ -9,8 +9,8 @@ def index_matrix(keywords,lines):
         indices.append([])
     for i in range(len(lines)):
         for j in range(len(keywords)):
-			if(keywords[j] in lines[i]['title']):
-				indices[j].append(i)
+		if(keywords[j] in lines[i]['title']):
+			indices[j].append(i)
     return indices
 
 # Given a list "l" and a list of indices "a". The function returns a subset of 
